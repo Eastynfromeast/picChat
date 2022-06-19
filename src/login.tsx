c59dd94220242cb3fb0f23a43fba59db30e0402f
Dongyeon Suh <eastyn@keefit.com>
2022-06-19 23:21:42 +0900

    I copy and pasted the code but don't know how to use it

--- src/login.tsx
+++ src/login.tsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+
+const auth = getAuth();
+createUserWithEmailAndPassword(auth, email, password)
+	.then(userCredential => {
+		// Signed in
+		const user = userCredential.user;
+		// ...
+	})
+	.catch(error => {
+		const errorCode = error.code;
+		const errorMessage = error.message;
+		// ..
+	});
 
 function Login() {
 	return (
